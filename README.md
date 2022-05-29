c424a2df132988bf77280936f08bc5bd896662f9
Lucas Robson <83253717+LukeRobs@users.noreply.github.com>
2022-05-28 22:51:13 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Oi, vamos jogar um jogo da memoria? 🙃
+
+[![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)](https://github.com/Naereen/StrapDown.js/blob/master/LICENSE)
+[![made-with-javascript](https://img.shields.io/badge/Made%20with-JavaScript-1f425f.svg)](https://www.javascript.com)
+![Badge em Desenvolvimento](http://img.shields.io/static/v1?label=STATUS&message=EM%20DESENVOLVIMENTO&color=GREEN&style=for-the-badge)
+![GitHub Orgs stars](https://img.shields.io/github/stars/LukeRobs?style=social)
+
+## Descrição do Projeto
+repositório do projeto de Javascript do bootcamp TQI full stack - Dio, 
+onde desenvolvi um jogo da memória com a temática de Super Mario! 
+
+### Imagens
+
+## 📁 Acesso ao projeto
+
+<p>Você pode Acessar o código fonte do projeto inicial ou baixá-lo</p>
+
+## Tecnologias utilizadas
+
+![Badge JS](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black)
+![Badge HTML](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)
+![Badge CSS](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)
---
+++ script.js
@@ -0,0 +1,66 @@
+const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+
+// Função para virar a carta
+function flipCard() {
+    if(lockBoard) return;
+    if(this === firstCard) return;
+
+    this.classList.add('flip');
+    if(!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMath()
+}
+//checa se as cartas são iguais
+function checkForMath() {
+    if(firstCard.dataset.card === secondCard.dataset.card) {
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard)
+    secondCard.removeEventListener('click', flipCard)
+
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1500);
+}
+
+/* Reseta o Tabuleiro*/
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null]
+}
+
+/* Embaralhar as cartas*/
+(function shuffle() {
+    cards.forEach((card) => {
+        let randomPosition = Math.floor(Math.random() * 12);
+        card.style.order = randomPosition;
+    })
+})();
+
+cards.forEach((card) => {
+    card.addEventListener('click', flipCard)
+});
